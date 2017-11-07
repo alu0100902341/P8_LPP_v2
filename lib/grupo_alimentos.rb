c32950f47939a5f311339cd0819603ffd5311f4e
SergioRavelo <alu0100902341@ull.edu.es>
2017-11-07 12:16:52 +0000

    Arreglado el spec y la clase grupo_alimentos

--- lib/grupo_alimentos.rb
+++ lib/grupo_alimentos.rb
@@ -2,5 +2,12 @@ require "./lib/lista"
 
 class Grupo_alimentos < Lista
 
+	def initialize(nombres_grupos, lista_alimentos)
+
+		@nombres_alimentos = Lista.new(nombres_grupos)
+		@alimentos = Lista.new(lista_alimentos)
+
+	end
+
 
 end
--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 require "./lib/alimento"
 require "./lib/lista"
+require "./lib/grupo_alimentos"
 
 RSpec.describe Gema do
   it "has a version number" do
@@ -46,50 +47,70 @@ RSpec.describe Gema do
 
   end
 
-  describe List do
+  describe Lista do
 
 	before :each do
 
-		@nodo = List.new(0, 1, 5)
-		@lista = [List.new(0, 1, 5) ,List.new(1, 2, 0), List.new(2, 3, 1), List.new(3, 4, 2), List.new(4, 5, 3), List.new(5, 0, 4) ]
+		@lista = [Nodo.new(0, 1, 5) ,Nodo.new(1, 2, 0), Nodo.new(2, 3, 1), Nodo.new(3, 4, 2), Nodo.new(4, 5, 3), Nodo.new(5, 0, 4) ]
+		@lista_dl = Lista.new(@lista)
 
 	end
 
 	it "Debe existir un nodo con su valor." do
-	   expect(@nodo.value).to eq(0)
+	   expect(@lista_dl.array[0].value).to eq(0)
 	 end
 
 	it "Debe existir un nodo con su siguiente." do
-	   expect(@nodo.next).to eq(1)
+	   expect(@lista_dl.array[0].next).to eq(1)
 	 end
 
 	it "Debe existir un nodo con su previo." do
-	   expect(@nodo.prev).to eq(5)
+	   expect(@lista_dl.array[0].prev).to eq(5)
 	 end
 
 	it "Debe existir una lista con su cabeza y su cola." do
-	   expect(@lista[0].prev).to eq(@lista[-1].value)
-	   expect(@lista[-1].next).to eq(@lista[0].value)
+	   expect(@lista_dl.head.prev).to eq(@lista_dl.tail.value)
+	   expect(@lista_dl.tail.next).to eq(@lista_dl.head.value)
 	 end
 
 	it "Se puede insertar un elemento en la Lista." do
-	   @lista << List.new(6, @lista[0].value, @lista[-1].value)
-	   @lista[0].prev = @lista[-1].value
-	   @lista[-2].next = @lista[-1].value
-	   expect(@lista[-1].value).to eq(6)
+	   expect(@lista_dl.push_back(Nodo.new(6, @lista_dl.head.value, @lista_dl.tail.value))).to eq(6)
 	 end
 
 	it "Se puede extraer el primer elemento de la lista." do
-	   expect(@lista.shift).to eq(@nodo)
+	   expect(@lista_dl.pop_front).to eq(1)
 	 end
 
 	it "Se puede extraer el último elemento de la lista." do
-	   nodo_aux = @lista[-2]
-	   @lista.pop
-	   expect(@lista[-1]).to eq(nodo_aux)
+	   expect(@lista_dl.pop).to eq(4)
 	 end
 
 
-  end
+end
+
+
+  describe Grupo_alimentos do
+
+	before :each do
+
+		@grupos = [ "Huevos, lácteos y helados", "Carnes y derivados", "Pescados y mariscos", "ALimentos grasos", "Alimentos ricos en carbohidratos", "Verduras y Hortalizas", "Frutas" ]
+
+		@gp1 = List.new( [ Alimento.new("Huevo frito", 14.1, 0.0, 19.5), Alimento.new("Leche de vaca", 3.3, 4.8, 3.2), Alimento.new("Yogurt", 3.8, 4.9, 3.8) ] )
+
+		@gp2 = List.new( [ Alimento.new("Cerdo", 21.5, 0.0, 6.3), Alimento.new("Ternera", 21.1, 0.0, 3.1), Alimento.new("Pollo", 20.6, 0.0, 5.6) ] )
+
+		@gp3 = List.new( [ Alimento.new("Bacalao", 17.7, 0.0, 0.4), Alimento.new("Atún", 21.5, 0.0, 15.5), Alimento.new("Salmón", 19.9, 0.0, 13.6) ] )
+
+		@gp4 = List.new( [ Alimento.new("Aceite de oliva", 0.0, 0.2, 99.6), Alimento.new("Mantequilla", 0.7, 0.0, 83.2), Alimento.new("Chocolate", 5.3, 47.0, 30.0) ] )
+
+		@gp5 = List.new( [ Alimento.new("Azúcar", 0.0, 99.8, 0.0), Alimento.new("Arroz", 6.8, 77.7, 0.6), Alimento.new("Lentejas", 23.5, 52.0, 1.4), Alimento.new("Papas", 2.0, 15.4, 0.1) ] )
+
+		@gp6 = List.new( [ Alimento.new("Manzana", 0.3, 12.4, 0.4), Alimento.new("Plátanos", 1.2, 21.4, 0.2), Alimento.new("Pera", 0.5, 12.7, 0.3) ] )
+
+		@grupos_alimentos = Grupos_alimentos.new( @grupos, [ @gp1, @gp2, @gp3, @gp4, @gp5, @gp6 ])
+	end
+
+
+end
 
 end
