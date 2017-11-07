d98441b1f9416804f052f1db37d814d021e4e152
SergioRavelo <alu0100902341@ull.edu.es>
2017-11-07 11:34:19 +0000

    Necesaria creación clase Grupo_alimentos mediante herencia

--- lib/lista.rb
+++ lib/lista.rb
@@ -1,4 +1,50 @@
 # create a Struct with :value, :next and :prev
-List = Struct.new(:value, :next, :prev) do
+Nodo = Struct.new(:value, :next, :prev) do
 
 end
+
+class Lista < Nodo
+
+attr_accessor :array, :head, :tail
+
+  def initialize(vector)	
+
+	@array = vector
+	@head = @array[0]
+	@tail = @array[-1]
+  end
+
+  def push_back(elemento)
+
+	@array << elemento
+	@array[0].prev = @array[-1].value
+	@array[-2].next = @array[-1].value
+	@tail = elemento
+	return tail.value
+  end
+
+
+  def pop_front
+
+	@array[-1].next = @array[1].value
+	@array[1].prev = @array[-1].value
+	@array.shift
+	@head = @array[0]
+	@tail = @array[-1]
+	return @head.value
+
+  end
+
+  def pop
+
+	@array[-2].next = @array[0].value
+	@array[0].prev = @array[-2].value
+	@array.pop
+	@head = @array[0]
+	@tail = @array[-1]
+	return @tail.value
+
+  end
+
+end
+
